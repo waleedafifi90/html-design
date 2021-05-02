d4ac796b9d3026074f211f27277a1f7bf4978286
Waleed A. Afifi <waleedafifich@gmail.com>
2021-05-02 14:00:13 +0300

    card page add card.html, csrd.css, add new function for the header in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
 'use strict';
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop + 100;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
